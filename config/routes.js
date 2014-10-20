f7d60ffca83d2b3446cf6a0d4bb4a2143c5bcd63
Eric Choi <ericchoi@Erics-MBP.local.tld>
2014-10-20 14:50:39 -0700

    fixed routes.js

--- config/routes.js
+++ config/routes.js
@@ -11,41 +11,41 @@ module.exports = function Routes(app, io){
       socket.on('in_all_posts', function(data){
         console.log('IN ALL POSTS', socket.rooms);
       });
-        socket.on('client:join_room', function(data) {
-          if (socket.rooms.length > 1 && socket.rooms[1] != data.room) {
-            console.log('LEAVING ROOM', socket.rooms[1]);
-            socket.leave(socket.rooms[1]);
-            socket.join(data.room);
-          } else {
-            socket.join(data.room);
-          }
-        });
+      socket.on('client:join_room', function(data) {
+        if (socket.rooms.length > 1 && socket.rooms[1] != data.room) {
+          console.log('LEAVING ROOM', socket.rooms[1]);
+          socket.leave(socket.rooms[1]);
+          socket.join(data.room);
+        } else {
+          socket.join(data.room);
+        }
+      });
 
-        socket.on('client:change_room', function(data) {
-            console.log('Client has left', data.prev_room);
-            console.log('Client has joined', data.new_room);
-            socket.leave(data.prev_room);
-            socket.join(data.new_room);
-        });
+      socket.on('client:change_room', function(data) {
+          console.log('Client has left', data.prev_room);
+          console.log('Client has joined', data.new_room);
+          socket.leave(data.prev_room);
+          socket.join(data.new_room);
+      });
 
-        socket.on('client:emit_message', function(data) {
-            console.log('Client has emitted this message', data);
-            io.to(data.room).emit('server:incoming_message', { name: data.name, message: data.message });
-        });
+      socket.on('client:emit_message', function(data) {
+          console.log('Client has emitted this message', data);
+          io.to(data.room).emit('server:incoming_message', { name: data.name, message: data.message });
+      });
 
-        socket.on('client:give_joy', function(data) {
-          console.log('client clicked on', data.id);
-          socket.broadcast.emit('server:update_joys', { post: data.id });
-        });
+      socket.on('client:give_joy', function(data) {
+        console.log('client clicked on', data.id);
+        socket.broadcast.emit('server:update_joys', { post: data.id });
+      });
 
-        socket.on('client: limbo_room', function (data){
-          io.to(data.post_id).emit('server: expired_room')
-        });
+      socket.on('client:limbo_room', function (data){
+        io.to(data.room_number).emit('server:expired_room')
+      });
 
-        socket.on('disconnect', function(data) {
-          socket.disconnect();
-          socket.leave(data.room);
-        });
-    });
+      socket.on('disconnect', function(data) {
+        socket.disconnect();
+        socket.leave(data.room);
+      });
+  });
 };
 
