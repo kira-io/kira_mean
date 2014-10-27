ed9be7cfa270834a8a4ff4ac785d0133e7628ffb
Eduardo <edubaik@gmail.com>
2014-10-27 00:48:24 -0700

    body pareser middleware

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ var express = require('express')
   , request = require('request')
   , isbot = require('is-bot')
   , externalip = require('externalip');
+var bodyParser = require('body-parser');  
 // configure environments
 app.configure(function(){
  	app.set('views', path.join(__dirname, 'views'));
  	app.set('view engine', 'ejs');
  	app.use(express.favicon());
  	app.use(express.logger('dev'));
- 	app.use(express.json());
- 	app.use(express.urlencoded());
- 	app.use(express.bodyParser()); //to allow handling of POST data
+  app.use(bodyParser());
+ 	// app.use(express.json());
+ 	// app.use(express.urlencoded());
+ 	// app.use(express.bodyParser()); //to allow handling of POST data
  	app.use(express.cookieParser()); //to allow session handling
  	app.use(express.session({secret: 'monkey'})); //for using sessions
  	app.use(app.router);
